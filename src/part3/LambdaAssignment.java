96a0b2ec4456ab602feeb69de28cde08ec8e6970
git-gabri <eleirbagm@gmail.com>
2021-07-08 19:41:13 +0200

    secondo

--- src/part3/LambdaAssignment.java
+++ src/part3/LambdaAssignment.java
@@ -1,10 +1,9 @@
 package part3;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class LambdaAssignment {
@@ -13,9 +12,30 @@ public class LambdaAssignment {
 
     public static void main(String[] args) {
         trasformaStudenti(studenti);
+        trasformaNumeri2(intro());
     }
     public static void trasformaStudenti(List<String> studenti){
         studenti.stream().filter(stu1 -> stu1.length() >= 5).map(String::toUpperCase).forEach(System.out::println);
+    }
+
+    public static void trasformaNumeri(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dammi dei numeri interi separati da virgola : (es. numero, numero)");
+        Stream<String> numeriStringa = Arrays.stream(input.nextLine().trim().split(","));
+        IntStream numeriInt = numeriStringa.mapToInt(Integer::parseInt);
+        OptionalDouble tot = numeriInt.distinct().map(num->num*num).average();
+        System.out.println(tot.getAsDouble());
+
+    }
+
+    public static void trasformaNumeri2(String riga){
+        System.out.println(Arrays.stream(riga.trim().split(",")).mapToInt(Integer::parseInt).distinct().map(num->num*num).average().getAsDouble());
+    }
+
+    public static String intro(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dammi dei numeri interi separati da virgola : (es. numero, numero)");
+        return input.nextLine();
     }
 
 }
